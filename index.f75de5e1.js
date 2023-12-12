ead5e63a395523c3853c14a1ea5b9d289b856cb5
Nataliia Henda <nataliehenda1296@gmail.com>
2023-12-12 17:32:38 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const div = document.createElement("div");
+    const h2 = document.createElement("h2");
+    const p = document.createElement("p");
+    div.className = `notification ${type}`;
+    h2.className = "title";
+    h2.textContent = title;
+    p.textContent = description;
+    div.style.top = `${posTop}px`;
+    div.style.right = `${posRight}px`;
+    div.append(h2);
+    div.append(p);
+    document.body.append(div);
+    setTimeout(()=>{
+        div.remove();
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
